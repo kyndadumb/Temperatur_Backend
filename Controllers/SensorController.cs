8f801ac47eb4709d238b8ed89ff12fb48ae64ca5
kyndadumb <46259262+kyndadumb@users.noreply.github.com>
2023-11-02 10:53:22 +0100

    Endpunkt für Löschung von Sensoren erstellt

--- Controllers/SensorController.cs
+++ Controllers/SensorController.cs
@@ -45,5 +45,41 @@ namespace API.Controllers
             }
             catch (Exception ex) { return StatusCode(StatusCodes.Status500InternalServerError, $"Bei der Erstellung des Sensors ist ein Fehler aufgetreten:\n--> {ex.Message}"); }
         }
+
+        [HttpPost("delete_sensor")]
+        public IActionResult SensorDeletion([FromBody] Sensors_Deletion sensor)
+        {
+            // Variablen
+            string connectionString = _configuration.GetConnectionString("mysqlConnection");
+            string output;
+
+            try
+            {
+                // konnte Connection-String erfolgreich gelesen werden?
+                Shared_Tools.Assert(!string.IsNullOrEmpty(connectionString), "Fehler beim Parsen der DB-Verbindungsinformationen");
+
+                // Datenbankverbindung eröffnen
+                using MySqlConnection connection = new(connectionString);
+                connection.Open();
+
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT SensorID FROM sensors WHERE SensorID = @SensorID";
+                command.Parameters.AddWithValue("@SensorID", sensor.ID);
+
+                MySqlDataReader reader = command.ExecuteReader();
+                output = Shared_Tools.SqlDataReader_ReadNullableString(reader, 0);
+
+                if (output == null) { return BadRequest($"Ein Sensor mit der ID {sensor.ID} ist nicht vorhanden!"); }
+
+                command = null;
+
+                command.CommandText = "DELETE FROM sensors WHERE SensorID = @SensorID";
+                command.Parameters.AddWithValue("@SensorID", sensor.ID);
+                command.ExecuteNonQuery();
+
+                return Ok($"Der Sensor (ID: {sensor.ID}) wurde entfernt!");
+            }
+            catch (Exception ex) { return StatusCode(StatusCodes.Status500InternalServerError, $"Bei der Erstellung des Sensors ist ein Fehler aufgetreten:\n--> {ex.Message}"); }
+        }
     }
 }
---
+++ Models/Sensors_Deletion.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Models
+{
+    public class Sensors_Deletion
+    {
+        public string ID { get; set; }
+    }
+}
